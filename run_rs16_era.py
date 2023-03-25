cf89e166788a42649a7463f1f01fd3476c59151a
Mitchell Krawiec-Thayer <mitchellpkt+github@protonmail.com>
2023-03-25 14:36:50 -0600

    chore: add time to output filename

--- run_rs16_era.py
+++ run_rs16_era.py
@@ -4,11 +4,11 @@ end_height: int = 2693928
 from interface_logic import RunConfig
 import server_config
 from loguru import logger
-from time import perf_counter
-
+import time
 
 logger.info("Starting ring size 16 era run...")
-tic: float = perf_counter()
+output_file_path: str = f"/home/user/isthmus_dev/output/rs16_era_{int(time.time())}.csv"
+tic: float = time.perf_counter()
 r = RunConfig(
     url=server_config.url,  # Replace these with your URL and port
     port=server_config.port,  # Replace these with your URL and port
@@ -18,7 +18,9 @@ r = RunConfig(
     transaction_batch_size=250,
     start_height=start_height,
     end_height=end_height,
-    save_to_csv="/home/user/isthmus_dev/output/rs16_era.csv",
+    save_to_csv=output_file_path,
     run_on_init=True,
 )
-logger.info(f"\nDone! Took {perf_counter() - tic:.2f} seconds to run over {end_height-start_height} blocks.")
+logger.info(f"\nDone!")
+logger.info(f"Took {time.perf_counter() - tic:.2f} seconds to run over {end_height-start_height} blocks.")
+logger.info(f"Output saved to {output_file_path}")
