81fc34f437775590cfb6930c3fc473e69208a845
Mitchell Krawiec-Thayer <mitchellpkt+github@protonmail.com>
2023-03-25 13:56:20 -0600

    chore: more logging, settings at top

--- run_rs16_era.py
+++ run_rs16_era.py
@@ -1,7 +1,11 @@
+start_height: int = 2688888
+end_height: int = 2693928
+
 from interface_logic import RunConfig
 import server_config
 from loguru import logger
 from time import perf_counter
+
 
 logger.info("Starting ring size 16 era run...")
 tic: float = perf_counter()
@@ -10,9 +14,9 @@ r = RunConfig(
     port=server_config.port,  # Replace these with your URL and port
     verbose=True,
     num_workers=16,
-    start_height=2688888,
-    end_height=None,  # 2683848  # < 1 weekish
+    start_height=start_height,
+    end_height=end_height,
     save_to_csv="/home/user/isthmus_dev/output/rs16_era.csv",
     run_on_init=True,
 )
-logger.info(f"\nDone! Took {perf_counter() - tic:.2f} seconds to run.")
+logger.info(f"\nDone! Took {perf_counter() - tic:.2f} seconds to run over {end_height-start_height} blocks.")
